046b0cf394411b1d90ee29d56df6ab3e8a12e1c6
bonsng <113011060+bonsng@users.noreply.github.com>
2025-03-21 13:21:54 +0900

    [Silver III] Title: 주유소, Time: 180 ms, Memory: 20352 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,22 @@
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+  .readFileSync(filepath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+const distances = input.shift().split(" ").map(Number);
+const prices = input.shift().split(" ").map(Number);
+
+prices.pop();
+
+let currentPrice = prices[0];
+let total = currentPrice * distances[0];
+
+for (let i = 1; i < N - 1; i++) {
+  if (currentPrice > prices[i]) currentPrice = prices[i];
+  total += currentPrice * distances[i];
+}
+
+console.log(total);
