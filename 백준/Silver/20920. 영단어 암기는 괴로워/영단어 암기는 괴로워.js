b7dd69ec1b82d05f75aec6688b79d371f11e44e7
bonsng <113011060+bonsng@users.noreply.github.com>
2025-03-23 16:05:09 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 448 ms, Memory: 53576 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.js
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.js
@@ -1,15 +1,21 @@
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const fs = require("fs");
-const [NnM, ...words] = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-const [n, m] = NnM.split(" ").map(Number);
+const [NnM, ...input] = fs.readFileSync(filepath).toString().trim().split("\n");
 
-const list = [];
+const [N, M] = NnM.split(" ").map(Number);
+const words = [];
 const dict = {};
+input.forEach((word) => {
+  if (word.length >= M) {
+    if (dict[word]) {
+      dict[word]++;
+    } else {
+      dict[word] = 1;
+      words.push(word);
+    }
+  }
+});
 
-// 사전 순 정렬
 const setOrder = (a, b, n) => {
   for (let i = 0; i < n; i++) {
     if (a[i] !== b[i]) {
@@ -18,33 +24,14 @@ const setOrder = (a, b, n) => {
   }
 };
 
-words.forEach((e) => {
-  if (e.length >= m) {
-    if (dict[e]) {
-      dict[e]++;
-    } else {
-      dict[e] = 1;
-      list.push(e);
-    }
-  }
-});
-
-list.sort((a, b) => {
-  // 빈도수
+words.sort((a, b) => {
   if (dict[a] < dict[b]) return 1;
-  
   else if (dict[a] === dict[b]) {
-    // 빈도수가 같다면 길이를 비교
     if (a.length < b.length) return 1;
-    
     else if (a.length === b.length) {
-      // 길이까지 같다면 사전 순으로 나열
       return setOrder(a, b, a.length);
-      
     } else return -1;
-    
   } else return -1;
 });
 
-
-console.log(list.join("\n"));
+console.log(words.join("\n"));
