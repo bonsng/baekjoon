575c6dcfb50c6c56ea40ab06d3df8370b5bba0ab
bonsng <113011060+bonsng@users.noreply.github.com>
2025-03-24 20:17:32 +0900

    [Silver II] Title: 비슷한 단어, Time: 92 ms, Memory: 9552 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
@@ -1,21 +1,47 @@
-const [N, ...wordList] = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const fs = require("fs");
+const [N, S, ...words] = fs
+  .readFileSync(filepath)
   .toString()
   .trim()
-  .split('\n');
+  .split("\n");
 
-const target = wordList.shift();
 let answer = 0;
 
-for (let word of wordList) {
-  if (
-    Math.abs(target.length - word.length) > 1 ||
-    Math.abs(new Set(target).size - new Set(word).size) > 1
-  )
-    continue;
+if (N > 1) {
+  for (const word of words) {
+    if (Math.abs(new Set(S).size - new Set(word).size) < 2) {
+      switch (S.length - word.length) {
+        case 0:
+          if (minus(word) < 2) answer++;
+          break;
+        case 1:
+          if (minus(word) == 1) answer++;
+          break;
+        case -1:
+          if (minus(word) == 0) answer++;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
 
-  for (const char of target) word = word.replace(char, '');
-  if (word.length < 2) answer++;
+function minus(word) {
+  let tempS = S;
+  for (const w of word) {
+    tempS = tempS.replace(w, "");
+  }
+  return tempS.length;
+}
+
+function rMinus(word) {
+  let tempW = word;
+  for (const w of S) {
+    tempW = tempW.replace(w, "");
+  }
+  return tempW.length;
 }
 
 console.log(answer);
