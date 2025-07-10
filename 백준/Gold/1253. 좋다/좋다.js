a6143b439db8c5a4d2d30e713b44e50dd63970d3
Bonseung Koo <113011060+bonsng@users.noreply.github.com>
2025-07-10 16:14:44 +0900

    [Gold IV] Title: 좋다, Time: 180 ms, Memory: 10024 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.js
+++ 백준/Gold/1253. 좋다/좋다.js
@@ -1,39 +1,39 @@
 const fs = require("fs");
-const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filepath).toString().trim().split("\n");
+const path = process.platform === "linux" ? "/dev/stdin" : "Wiki\\input.txt";
+const inputs = fs.readFileSync(path).toString().trim().split("\n");
 
-const N = +input.shift();
-const nums = input[0].split(" ").map(Number);
-const set = new Set();
-const numState = {};
+const n = Number(inputs[0]);
+const A = inputs[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-for (const n of nums) {
-  if (numState[n]) {
-    numState[n] += 1;
-  } else {
-    numState[n] = 1;
-  }
-}
+function isGood(target) {
+  let left = 0;
+  let right = A.length - 1;
 
-for (let i = 0; i < N; i++) {
-  for (let j = i + 1; j < N; j++) {
-    if (nums[i] === 0 && nums[j] === 0) {
-      if (numState[0] >= 3) set.add(0);
-    } else if (nums[i] === 0) {
-      if (numState[nums[j]] >= 2) set.add(nums[j]);
-    } else if (nums[j] === 0) {
-      if (numState[nums[i]] >= 2) set.add(nums[i]);
-    } else {
-      set.add(nums[i] + nums[j]);
+  while (left < right) {
+    const sum = A[left] + A[right];
+
+    if (left === target) {
+      left += 1;
+      continue;
+    } else if (right === target) {
+      right -= 1;
+      continue;
     }
+
+    if (sum === A[target]) return true;
+    else if (sum < A[target]) left += 1;
+    else right -= 1;
   }
+
+  return false;
 }
 
-let answer = 0;
-for (const n of nums) {
-  if (set.has(n)) {
-    answer += 1;
-  }
+let ans = 0;
+for (let i = 0; i < A.length; i++) {
+  if (isGood(i)) ans += 1;
 }
 
-console.log(answer);
+console.log(ans);
