c8a363beacae8382d906cf33cb6476210e88f794
Bonseung Koo <113011060+bonsng@users.noreply.github.com>
2025-07-16 21:58:49 +0900

    [Gold IV] Title: 문자열 폭발, Time: 308 ms, Memory: 78804 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+let arr = input[0];
+let bomb = input[1];
+let bomLen = bomb.length;
+let stack = [];
+let top = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  stack.push(arr[i]);
+  top = arr[i];
+  if (top === bomb[bomLen - 1]) {
+    let last = stack.slice(-bomLen);
+    if (last.join("") === bomb) stack.splice(-bomLen);
+  }
+}
+console.log(stack.length ? stack.join("") : "FRULA");
