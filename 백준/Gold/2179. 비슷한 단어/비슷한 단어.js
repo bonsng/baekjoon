2395883d46edea53eb11a9940a0c19fbd87b4b94
Bonseung Koo <113011060+bonsng@users.noreply.github.com>
2025-07-18 16:38:22 +0900

    [Gold IV] Title: 비슷한 단어, Time: 512 ms, Memory: 15440 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.js
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.js
@@ -1,26 +1,63 @@
-const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const fs = require("fs");
-const [N, ...input] = fs.readFileSync(filepath).toString().trim().split("\n");
-function compareWord(word1, word2) {
-  let prefix = 0;
-  let wordLength = word1.length > word2.length ? word2.length : word1.length;
+let N = 0;
 
-  for (let i = 0; i < wordLength; i++) {
-    if (word1.charCodeAt(i) !== word2.charCodeAt(i)) return prefix;
-    prefix++;
-  }
-  return prefix;
-}
-let answer = -1;
-let answerWords = [];
-for (let i = 0; i < +N; i++) {
-  for (let j = i + 1; j < +N; j++) {
-    const prefixLength = compareWord(input[i], input[j]);
-    if (prefixLength > answer) {
-      answer = prefixLength;
-      answerWords = [input[i], input[j]];
+const solution = (input) => {
+  input.sort((a, b) => {
+    return a.value > b.value ? 1 : -1;
+  });
+
+  let max = -Infinity;
+  let ans = [];
+  for (let i = 0; i < N - 1; i++) {
+    for (let j = i + 1; j < N; j++) {
+      const { key: idxI, value: StringI } = input[i];
+      const { key: idxJ, value: StringJ } = input[j];
+
+      if (StringI.charAt(0) !== StringJ.charAt(0)) break;
+      const len = getSameWordLength(StringI, StringJ);
+      if (max < len) {
+        max = len;
+        const [minKey, maxKey] = [Math.min(idxI, idxJ), Math.max(idxI, idxJ)];
+
+        ans = [minKey, maxKey];
+      } else if (max === len) {
+        const [minKey, maxKey] = [Math.min(idxI, idxJ), Math.max(idxI, idxJ)];
+
+        if (ans[0] > minKey || (ans[0] === minKey && ans[1] > maxKey)) {
+          ans = [minKey, maxKey];
+        }
+      }
     }
   }
-}
+  console.log(input.find((object) => object.key === ans[0]).value);
+  console.log(input.find((object) => object.key === ans[1]).value);
+};
 
-answerWords.forEach((e) => console.log(e));
+const getSameWordLength = (str1, str2) => {
+  const len = Math.min(str1.length, str2.length);
+  let cnt = 0;
+
+  for (let i = 0; i < len; i++, cnt++) {
+    if (str1.charAt(i) !== str2.charAt(i)) {
+      return cnt;
+    }
+  }
+  return cnt;
+};
+input = [];
+let index = 0;
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", (line) => {
+    if (N === 0) {
+      N = parseInt(line);
+      return;
+    }
+    input.push({
+      key: index++,
+      value: line,
+    });
+  })
+  .on("close", () => {
+    solution(input);
+    process.exit();
+  });
